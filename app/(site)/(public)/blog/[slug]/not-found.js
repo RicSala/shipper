e67519fcd2ba368172ef0bbfe32014c713b01cc1
RicSala <ricsala86@gmail.com>
2023-10-23 11:52:31 +0200

    fix not found component

--- app/(site)/(public)/blog/[slug]/not-found.js
+++ app/(site)/(public)/blog/[slug]/not-found.js
@@ -1,6 +1,25 @@
 // This file handles the not-found errors thrown in all the app (using the notFound()) AND any unmatched URL in the whole application. If you need more specific not-found pages, you can create specific pages for each route (For example, )
 
 import NotFoundSection from "@/components/not-found";
+import { Bricolage_Grotesque, Inter, Roboto_Mono } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
+
+export const roboto_mono = Roboto_Mono({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto-mono",
+});
+
+export const bricolage = Bricolage_Grotesque({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-bricolage",
+});
 
 export default function NotFound() {
   return (
--- shipper.config.js
+++ shipper.config.js
@@ -1,6 +1,6 @@
 export const config = {
   general: {
-    appName: "Shipper",
+    appName: "CoverAi",
   },
 
   colors: {
